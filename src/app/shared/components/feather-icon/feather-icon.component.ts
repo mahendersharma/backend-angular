fb3da0a97c4bf662560726a67d20a520b7be6041
Mahender Sharma <72732952+mahendersharma@users.noreply.github.com>
2022-05-21 13:28:05 +0530

    icons-daynmic

--- src/app/shared/components/feather-icon/feather-icon.component.ts
+++ src/app/shared/components/feather-icon/feather-icon.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import * as feather from 'feather-icons'
 
 @Component({
   selector: 'app-feather-icon',
   templateUrl: './feather-icon.component.html',
   styleUrls: ['./feather-icon.component.scss']
 })
-export class FeatherIconComponent implements OnInit {
-
+export class FeatherIconComponent implements OnInit, AfterViewInit {
+  @Input('icon') feathericon: any
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngAfterViewInit() {
+    feather.replace()
+
+  }
+
 
 }
